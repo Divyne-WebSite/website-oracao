475f3592643443ce6bd62a97f9314bb238f73b57
ViGF <vitoriogdef@gmail.com>
2023-03-18 16:15:09 -0300

    fix: navbar mobile

--- src/App.tsx
+++ src/App.tsx
@@ -6,56 +6,51 @@ import { menu } from 'ionicons/icons';
 import Body from './BodySite';
 
 function App() {
-  const [showMenu, setShowMenu] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  function toggleMenu() {
-    setShowMenu(!showMenu);
-  }
-
-  return (
-    <div>
-      <nav className="pg-9 md:flex md:items-center md:justify-between">
-        <div className="flex items-center justify-between">
-          <div className="md:flex md:items-center Parte-esquerda">
-            <img src={Logo} alt="logo" className="h-30 inline" />
-            <span className="text-2x1 font-inter ml-3">
-              <div className="flex flex-col">
-                <span className="text-black opacity-50 font-regular">
-                  Seja Bem-vindo a
+    return (
+        <div>
+            <nav className="flex items-center justify-between">
+                <div className="flex justify-center flex-row max-md:flex-col max-md:text-center items-center z-10 max-md:pl-5 relative">
+                    <img src={Logo} alt="logo" className="h-30 max-md:h-24 max-md:mt-4" />
+                    <div className="flex flex-col max-md:text-xs max-md:text-center font-inter max-md:absolute top-3/4 whitespace-nowrap">
+                        <span className="text-black opacity-50 font-regular">
+                            Seja Bem-vindo a
+                        </span>
+                        <span className="text-2x1 font-bold">
+                            7x Support Prayer
+                        </span>
+                    </div>
+                </div>
+                <div className={`flex flex-1 justify-end max-md:h-screen font-inter mr-8 max-md:w-screen max-md:absolute max-md:justify-center left-0 max-md:py-0 py-4 max-md:pl-0 pl-7 duration-500 items-center ${mobileMenuOpen ? 'top-0 bg-white' : '-top-full'}`}>
+                    <ul className='flex gap-12 max-md:gap-7 max-md:flex-col max-md:text-center text-xl'>
+                        <li>
+                            <a href="#" className="hover:text-indigo-900 duration-500">
+                                Início
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:text-indigo-900 duration-500">
+                                Sobre
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:text-indigo-900 duration-500">
+                                Ajuda
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <span
+                    className="pr-5 z-10 cursor-pointer md:hidden max-md:mt-14 block"
+                    onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+                    <span className="sr-only">Open main menu</span>
+                    <IonIcon icon={menu} size="large" />
                 </span>
-                <span className="text-2x1 font-bold">
-                  7x Support Prayer
-                </span>
-              </div>
-            </span>
-          </div>
-          <span className="text-3x1 cursor-pointer mx-2 md:hidden block" onClick={toggleMenu}>
-            <IonIcon icon={menu} size="large" />
-          </span>
+            </nav>
+            <Body />
         </div>
-        <ul
-          className={`md:flex md:items-center font-inter mr-8 z-[-1] md:z-auto md:static absolute left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 ${showMenu ? 'opacity-100 top-[-400] bg-white-500 z-10' : 'opacity-0 top-[-200]'} transition-all ease-in duration-500`}
-        >
-          <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-indigo-900 duration-500">
-              Início
-            </a>
-          </li>
-          <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-indigo-900 duration-500">
-              Sobre
-            </a>
-          </li>
-          <li className="mx-4 my-6 md:my-0">
-            <a href="#" className="text-xl hover:text-indigo-900 duration-500">
-              Ajuda
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <Body />
-    </div>
-  );
+    );
 }
 
 export default App;
