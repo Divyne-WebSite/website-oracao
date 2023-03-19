56ab27bda659a9491fa97848d7c7ecde434664e5
Erick <estudodoerick@gmail.com>
2023-03-19 01:39:49 -0300

    terminad_text

--- src/components/BodySite/index.tsx
+++ src/components/BodySite/index.tsx
@@ -3,22 +3,21 @@ import body from '../../assets/body.png'
 import './styles.css'
 
 function BodySite() {
-
-
   return (
-    <div className="flex font-inter items-start justify-start md:ml-28 ml-5">
-      <div className="flex flex-col mt-44">
+    <div className="flex font-inter items-start justify-start md:ml-16 ml-5">
+      <div id='container' className="flex flex-col mt-44">
         <span className="font-bold text-6xl">Aqui você tem </span>
         <span className="font-bold text-6xl">pessoas orando</span>
         <span className="font-bold text-6xl mb-10">por você</span>
-        <span className="font-regular text-3xl">Você não precisa nos falar nem o seu nome,</span>
-        <span className="font-bold text-3xl mb-10">apenas peça</span>
+        <div className='secoundText'>
+          <span>Você não precisa nos falar nem o seu nome, <strong>apenas peça</strong> </span>
+        </div>
         <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-md w-64 h-12">
           Peça sua oração
         </button>
 
       </div>
-      <img src={body} className="ml-auto mx-2 md:block hidden" />
+      <img id='image' src={body} />
     </div>
   );
 }
